e984c4b38a88b9f5211a38da2b05d42732356b3e
Matheus Marques <matheusm42@gmail.com>
2025-07-20 23:33:24 -0300

    add handle cycles

--- app/INets.hs
+++ app/INets.hs
@@ -31,6 +31,27 @@ step _ _ (INet terms ((Connection term (Wire w)):cs)) =
 step i rs (INet terms ((Connection t1 t2):cs)) =
     INet terms ((concat [indirection r i t1 t2| r<-rs])++cs)
 
+combineConn :: Connection -> INet -> INet
+combineConn c (INet ts cs) = INet ts (c:cs)
+
+deepSearch :: Term -> Term -> Bool
+deepSearch (Wire a) (Wire b) = a==b
+deepSearch (Tree s1 as) (Tree s2 bs) = Tree s1 as == Tree s2 bs
+deepSearch (Tree s as) (Wire b) = deepSearch (Wire b) (Tree s as)
+deepSearch (Wire b) (Tree t (a:as)) = case a of
+    (Wire x) -> x == b
+    (Tree _ xs) -> any (deepSearch (Wire b)) xs || deepSearch (Wire b) (Tree t as)
+deepSearch (Wire _) (Tree _ []) = False
+
+
+isCycle :: Connection -> Bool
+isCycle (Connection (Wire a) (Wire b)) = a==b
+isCycle (Connection (Tree _ _) (Tree _ _)) = False
+isCycle (Connection (Tree s xs) (Wire y)) = isCycle (Connection (Wire y) (Tree s xs))
+isCycle (Connection (Wire y) (Tree s xs)) = deepSearch (Wire y) (Tree s xs)
+
 runINet :: Int -> [Rule] -> INet -> INet
 runINet _ _ (INet t []) = INet t []
-runINet i rs inet = runINet (i+1) rs (step i rs inet)
+runINet i rs (INet t (c:cs)) = if isCycle c 
+    then combineConn c $ runINet i rs (INet t cs)
+    else runINet (i+1) rs (step i rs (INet t (c:cs)))
